89274717a3447e8c711667159a86c4d2a54df7f8
Geroge Fu <nailao_5918@outlook.com>
2018-10-13 13:35:02 +0800

    change RGB pins

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1865,9 +1865,9 @@
 //#define RGBW_LED
 
 #if ENABLED(RGB_LED) || ENABLED(RGBW_LED)
-  #define RGB_LED_R_PIN 34
-  #define RGB_LED_G_PIN 43
-  #define RGB_LED_B_PIN 35
+  #define RGB_LED_R_PIN 3
+  #define RGB_LED_G_PIN 4
+  #define RGB_LED_B_PIN 9
   #define RGB_LED_W_PIN -1
 #endif
 
